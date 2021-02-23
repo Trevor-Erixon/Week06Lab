04776922d383bcdfdbf78ae57f66385c83ce3780
Trevor-Erixon <Trevor-Erixon>
2021-02-23 15:03:48 -0700

    Added some basic display functionality Both JSPs display ShoppingListServlet correctly sets the username session attribute, but does not process any actions yet

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -17,12 +18,24 @@ public class ShoppingListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        HttpSession session = request.getSession();
+        
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        HttpSession session = request.getSession();        
+        String username = request.getParameter("username");
+        
+        session.setAttribute("username", username);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                .forward(request, response);
     }
 
 }
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -9,9 +9,19 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Shopping List</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Shopping List</h1>
+        <p>Hello, ${username} <a href="ShoppingList?action=logout">Logout</a></p>
+        <form name="addItemForm" method="post">
+            <h2>List</h2>
+            Add item: <input type="text" name="item"><input type="submit" value="Add Item">
+            <input type="hidden" name="action" value="add">
+        </form>
+        <form name="displayDeleteItemForm" method="post">
+            
+            
+        </form>
     </body>
 </html>
